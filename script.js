f2d4b14a43dd1ebbc11de6f21c493d46aa1a0b02
mooreEffort <alexmaksimets@gmail.com>
2022-11-28 20:38:50 +1000

    login modal, function and tone working, unable to get startup wallpaper to work

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
-// initial screen
-// blinker
+// DOM elements
+
+// init
+const bodyMain = document.querySelector("body");
 const blinker = document.querySelector(".blinker");
 const startDiv = document.querySelector(".start-div");
-const startWallpaper = document.querySelector(".start-screen__wallpaper");
+const startWallpaper = document.querySelector(".wallpaper-container");
+// login
+const loginModal = document.querySelector(".login-div__modal");
+const loginWindow = document.querySelector(".login-div");
+const loginForm = document.querySelector(".login-div__layout--form");
+const usernameInput = document.querySelector("#username");
+const passwordInput = document.querySelector("#password");
+const loginJingle = new Audio("./assets/sounds/win95-startup.mp3");
+
+// initial screen
+// blinker
+const showHide = (element, time, type, value) => {
+  window.setTimeout(() => {
+    element.style[type] = value;
+  }, time);
+};
 
 const initiate = () => {
   window.setInterval(() => {
@@ -12,28 +29,46 @@ const initiate = () => {
       : (blinker.style.visibility = "hidden");
   }, 400);
 
-  window.setTimeout(() => {
-    startDiv.style.display = "none";
-  }, 5000);
+  showHide(startDiv, 5000, "display", "none");
+
+  showHide(startWallpaper, 6000, "display", "block");
+
+  showHide(startWallpaper, 6000, "display", "none");
+
+  // window.setTimeout(() => {
+  //   bodyMain.removeChild(startWallpaper);
+  // }, 5000);
 
   window.setTimeout(() => {
-    startWallpaper.style.display = "block";
+    loginModal.style.display = "block";
+    bodyMain.classList.toggle("body-green");
   }, 6000);
 };
 
-// initiate();
+initiate();
+
+// play login jingle
+
+// user login function
+const login = () => {
+  showHide(loginWindow, 1000, "display", "none");
+  console.log(bodyMain);
+  // background.style.backgroundColor = "008081";
+
+  loginJingle.play();
+  // startWallpaper.src = "./";
+  // console.dir(startWallpaper);
+  // loginWindow.style.display = "none";
+};
 
 // Login validation
 const passKey = "maksimumeffort";
-const loginForm = document.querySelector(".login-div__layout--form");
-const usernameInput = document.querySelector("#username");
-const passwordInput = document.querySelector("#password");
 
 loginForm.addEventListener("submit", (event) => {
   event.preventDefault();
   if (usernameInput.value === passKey && passwordInput.value === passKey) {
-    console.log("log in successful");
+    login();
   } else {
-    console.log("incorrect login");
+    passwordInput.classList.toggle("error");
   }
 });
